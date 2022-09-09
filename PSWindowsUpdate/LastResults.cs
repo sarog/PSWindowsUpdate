4fd15af911a47bc8b0c26f4d071d223034adcf41
Saro G <sarog@users.noreply.github.com>
2022-09-09 17:37:52 -0400

    Switch over to PowerShellStandard.Library

--- PSWindowsUpdate/LastResults.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PSWindowsUpdate {
-    public class LastResults {
-        public string ComputerName { get; set; }
-
-        public string LastSearchSuccessDate { get; set; }
-
-        public string LastInstallationSuccessDate { get; set; }
-    }
-}
--- PSWindowsUpdate/RebootStatus.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PSWindowsUpdate {
-    public class RebootStatus {
-        public string ComputerName { get; set; }
-
-        public string RebootRequired { get; set; }
-
-        public string RebootScheduled { get; set; }
-    }
-}
